553bc2041e5384e73ea90a62ba1e6e9ab9ee64c7
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-11 21:48:17 +0200

    Moving code files into its own folder

---
+++ thorsky/__init__.py
@@ -0,0 +1,9 @@
+
+name = 'thorsky'
+
+__all__ = ["Arcsecond",
+           "ArcsecondError",
+           "ArcsecondConnectionError",
+           "ArcsecondInvalidEndpointError"]
+
+__version__ = '1.0.1'
